630dda8f454f37f5e99c4560cb918fe100dacfdb
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-04-29 00:17:30 -0500

    Refactored css code for diseño and added a graph to home page

--- Glastore/views/home.py
+++ Glastore/views/home.py
@@ -1,13 +1,20 @@
 from flask import (
     Blueprint, render_template
 )
+import matplotlib.pyplot as plt
+from Glastore.models import get_temporary_uri
 
 bp = Blueprint('home', __name__)
 
 
 @bp.route('/', methods=('POST', 'GET'))
 def home():
+    figure = plt.Figure(dpi=150, figsize=(4, 4))
+    axis = figure.subplots()
+    axis.plot([1, 2, 3], [1, 2, 3])
+    temporary_uri = get_temporary_uri(figure)
 
     return render_template(
-        'home/home.html'
+        'home/home.html',
+        my_plot=temporary_uri
     )
