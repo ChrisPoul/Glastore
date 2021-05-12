5dce791e12e8e2b88d3218de0ce3a496be46d059
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-05-11 21:34:02 -0500

    Refactored add view

--- Glastore/views/quote.py
+++ Glastore/views/quote.py
@@ -9,16 +9,11 @@ from Glastore.models import format_date, format_price
 from Glastore.views import get_form
 from Glastore.models.quote import Quote
 from Glastore.models.product import Product
-from Glastore.models.customer import Customer
+from Glastore.models.customer import Customer, customer_heads
 from Glastore.views.auth import login_required
 
 bp = Blueprint("quote", __name__, url_prefix="/quote")
 
-customer_heads = {
-    "name": "Cliente",
-    "email": "Email",
-    "address": "Dirección"
-}
 customer_placeholders = {
     "name": "Nombre del cliente...",
     "email": "Corréo electrónico...",
@@ -44,12 +39,6 @@ product_placeholders = {
 @bp.route('/add', methods=('GET', 'POST'))
 @login_required
 def add():
-    customer_heads = {
-        "name": "Cliente",
-        "email": "Email",
-        "phone": "Tel.",
-        "address": "Dirección"
-    }
     form = get_form(customer_heads)
     customer_names = []
     customer_emails = []
@@ -61,20 +50,16 @@ def add():
         customer_phones.append(customer.phone)
         customer_addresses.append(customer.address)
     if request.method == "POST":
-        for key in customer_heads:
-            customer = Customer.search(request.form[key])
-            if customer:
-                quote = Quote.new(customer.id)
-                return redirect(
-                    url_for('quote.edit', id=quote.id)
-                )
-        customer = Customer(
-            name=form['name'],
-            email=form['email'],
-            phone=form['phone'],
-            address=form['address']
-        )
-        error = customer.request.add()
+        error = None
+        customer = search_for_customer(form)
+        if not customer:
+            customer = Customer(
+                name=form['name'],
+                email=form['email'],
+                phone=form['phone'],
+                address=form['address']
+            )
+            error = customer.request.add()
         if not error:
             quote = Quote.new(customer.id)
             return redirect(
@@ -94,8 +79,22 @@ def add():
     )
 
 
+def search_for_customer(form):
+    for key in customer_heads:
+        customer = Customer.search(form[key])
+        if customer:
+            return customer
+
+    return None
+
+
 @bp.route("/edit/<int:id>", methods=('GET', 'POST'))
 def edit(id):
+    customer_heads = {
+        "name": "Cliente",
+        "email": "Email",
+        "address": "Dirección"
+    }
     quote = Quote.get(id)
     quote.done = False
     if request.method == "POST":
